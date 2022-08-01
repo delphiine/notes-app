cc2b24be6e6eaba71f96a5382909c447ebe4d0dc
delphine <delphine.mubiligi@gmail.com>
2022-08-01 15:32:58 +0100

    implemented the class 'NotesModel'

---
+++ notesModel.js
@@ -0,0 +1,19 @@
+class NotesModel {
+  constructor() {
+    this.list = [];
+  }
+
+  getNotes() {
+    return this.list;
+  }
+
+  addNote(note) {
+    this.list.push(note);
+  }
+
+  reset() {
+    this.list = [];
+  }
+}
+
+module.exports = NotesModel;
---
+++ notesModel.test.js
@@ -0,0 +1,23 @@
+const NotesModel = require("./notesModel")
+
+describe("NotesModel", () => {
+  it("should return an empty array",() => {
+    const model = new NotesModel();
+    expect(model.getNotes()).toEqual([]); 
+  })
+
+  it("adds items and returns an updated list", () => {
+    const model = new NotesModel();
+    model.addNote("Buy milk");
+    model.addNote("Go to the gym");
+    expect(model.getNotes()).toEqual(["Buy milk", "Go to the gym"]);
+  })
+
+  it("removes all items from the list", () => {
+    const model = new NotesModel();
+    model.addNote("Buy milk");
+    model.addNote("Go to the gym");
+    model.reset();
+    expect(model.getNotes()).toEqual([]);
+  })
+})
