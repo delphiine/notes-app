67d9d75e3a64c1d9685576b6a0986877a1a1f2d8
delphine <delphine.mubiligi@gmail.com>
2022-08-02 12:35:30 +0100

    changed web page content by using the displayNotes in 'index.js'

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+const NotesModel = require("./notesModel");
+const NotesView = require("./notesView");
+
 console.log('The notes app is running')
+
+const model = new NotesModel;
+model.addNote("This is an example note")
+
+const view = new NotesView(model);
+view.displayNotes()
