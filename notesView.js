06d0499cdb416d9215dabc4fd1258beb11b10f21
delphine <delphine.mubiligi@gmail.com>
2022-08-02 21:59:27 +0100

    fixed the two bugs of the challenge

--- notesView.js
+++ notesView.js
@@ -6,11 +6,16 @@ class NotesView {
     document.querySelector('#add-note-button').addEventListener('click', () => {
       const newNote = document.querySelector('#add-note-input').value;
       this.addNewNote(newNote);
+      document.querySelector('#add-note-input').value = ''
     })
   }
   
   displayNotes() {
     const notes = this.model.getNotes();
+
+    document.querySelectorAll('div.note').forEach(note => {
+      note.remove()
+    });
 
     notes.forEach(note => {
       const noteEl = document.createElement('div');
--- notesView.test.js
+++ notesView.test.js
@@ -2,33 +2,62 @@
  * @jest-environment jsdom
  */
 
- const fs = require('fs');
- const NotesModel = require('./notesModel');
- const NotesView = require('./notesView'); 
+const fs = require('fs');
+const NotesModel = require('./notesModel');
+const NotesView = require('./notesView'); 
  
- describe('Notes view', () => {
-   it('displays two notes', () => {
-     document.body.innerHTML = fs.readFileSync('./index.html');
-     const model = new NotesModel();
-     const view = new NotesView(model);
+describe('Notes view', () => {
+  it('displays two notes', () => {
+    document.body.innerHTML = fs.readFileSync('./index.html');
+    const model = new NotesModel();
+    const view = new NotesView(model);
 
-     model.addNote('Funny note');
-     model.addNote('An even funnier note');
-     view.displayNotes();
-     expect(view.displayNotes()).toContain('Funny note');
-     expect(document.querySelectorAll('div.note').length).toEqual(2);
-   });
+    const firstNote = 'Funny note'
+    const secondNote = 'An even funnier note'
+    model.addNote(firstNote);
+    model.addNote(secondNote);
+    view.displayNotes();
+    expect(document.querySelectorAll('div.note')[0].innerText).toContain(firstNote);
+    expect(document.querySelectorAll('div.note')[1].innerText).toContain(secondNote);
+    expect(document.querySelectorAll('div.note').length).toEqual(2);
+  });
 
-   it('adds a new note and displays it on page', () => {
+  it('displays added notes without duplicates', () => {
+    document.body.innerHTML = fs.readFileSync('./index.html');
+    const model = new NotesModel();
+    const view = new NotesView(model);
+
+    const firstNote = 'Funny note'
+    const secondNote = 'An even funnier note'
+    model.addNote(firstNote);
+    model.addNote(secondNote);
+    view.displayNotes();
+    expect(document.querySelectorAll('div.note')[0].innerText).toContain(firstNote);
+    expect(document.querySelectorAll('div.note')[1].innerText).toContain(secondNote);
+    expect(document.querySelectorAll('div.note').length).toEqual(2);
+
+    // adding a new Note and displaying again
+    const thirdNote = 'this is the funniest note'
+    model.addNote(thirdNote);
+    view.displayNotes();
+    expect(document.querySelectorAll('div.note')[0].innerText).toContain(firstNote);
+    expect(document.querySelectorAll('div.note')[1].innerText).toContain(secondNote);
+    expect(document.querySelectorAll('div.note')[2].innerText).toContain(thirdNote);
+    expect(document.querySelectorAll('div.note').length).toEqual(3);
+  });
+
+  it('adds a new note and displays it on page', () => {
     document.body.innerHTML = fs.readFileSync('./index.html')
     const model = new NotesModel();
-    const view = new NotesView();
+    const view = new NotesView(model);
 
     const input = document.querySelector('#add-note-input');
     const button = document.querySelector('#add-note-button')
 
-    input.value = 'Buy milk';
+    const noteValue = 'Buy milk'
+    input.value = noteValue;
     button.click();
-    expect(document.querySelectorAll('div.note')[0].innerText).toEqual('Buy milk');
-   });
- });
+    expect(document.querySelectorAll('div.note')[0].innerText).toEqual(noteValue);
+    expect(document.querySelector('#add-note-input').value).toEqual('');
+  });
+});
